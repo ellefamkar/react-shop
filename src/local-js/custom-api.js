c7afa3b0580b39e9deb50a04ac6fa130b45ed914
elnaz famkar <86571329+ellefamkar@users.noreply.github.com>
2023-02-22 22:01:35 +0330

    feat : add local js file and images

---
+++ src/local-js/custom-api.js
@@ -0,0 +1,28 @@
+export const Products = [
+    {
+        "id": 1,
+        "title": "The Spiral Stool",
+        "price": 319.95,
+        "description": "The opening shape in the middle in this design consisting mainly of three helical plywood elements",
+        "category": "Tables",
+        "image": "public/images/1.jpeg",
+        "rating": {
+        "rate": 6.9,
+        "count": 120
+        }
+    },
+    
+    {
+        "id": 2,
+        "title": "The Wavelet",
+        "price": 109.95,
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "category": "men's clothing",
+        "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+        "rating": {
+        "rate": 3.9,
+        "count": 120
+        }
+    },
+    
+]
--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 //we separate base utl to  use different endpoints easily
 const Base_URL = "https://fakestoreapi.com";
-
+// const Base_URL = "https://api.escuelajs.co/api/v1";
+// 
 // asynchronous function 
 const getProducts = async () =>{
     // send request to products endpoint
